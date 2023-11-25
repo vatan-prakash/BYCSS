ec0ba436f862e0d02a8e7403adb4d13f5ad3815b
Vatan Prakash <vatanprakash867@gmail.com>
2023-11-25 23:56:41 +0530

    Your meaningful commit message here

---
+++ src/app/Components/Navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./style.css";
+import { FormInputGroup } from "./Searchbar";
+
+export const Navbar = () => {
+  return (
+    <div className="box">
+     <FormInputGroup/>
+    </div>
+  );
+};
---
+++ src/app/Components/Navbar/Searchbar.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./style.css";
+
+export const FormInputGroup = () => {
+    return (
+        <div className="form-input-group">
+            <div className="text">Search</div>
+        </div>
+    );
+};
